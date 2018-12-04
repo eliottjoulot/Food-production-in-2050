86679a8a5ada2fdada29627a3e03fd1f07d32988
MaxencePPierre <mpetitpierre1@gmail.com>
2018-12-04 21:49:27 +0100

    Update Menu Selection

--- main.js
+++ main.js
@@ -58,14 +58,14 @@ function displayInfo() {
 		console.log('	previous :' + previousSSP);
 		document.getElementById(previousSSP).style.display = "none";
 	}
-	console.log("Display info : " + selectedSSP);
+	//console.log("Display info : " + selectedSSP);
 	document.getElementById(selectedSSP).style.display = "inline";
 	previousSSP = selectedSSP;	
 }
 
 function showLoading() {
 	// Adding loading gif
-	console.log('Adding loading gif');
+	console.log('	Adding loading gif');
 
 	var loadingDiv = document.createElement("div");
 	loadingDiv.id = "loading";
@@ -99,7 +99,7 @@ function showLoading() {
 function hideLoading() {
 	// Removing loading gif
 	//await sleep(2000);
-	console.log('Removing loading gif');
+	console.log('	Removing loading gif');
 	document.getElementById("loading").remove();
 }
 
@@ -119,8 +119,6 @@ function mapCreate() {
 }
 
 function mapRemove() {
-	
-
 	let mapId = 'earthquakes';
 	var mapLayer = map.getLayer(mapId);
 
@@ -130,80 +128,96 @@ function mapRemove() {
     }
 }
 
+function mapRemoveLayerOnly() {
+	let mapId = 'earthquakes';
+	var mapLayer = map.getLayer(mapId);
+	
+	console.log('dkzjdazfnlazkenf : ' + mapLayer);
+
+    if(typeof mapLayer !== 'undefined') {
+      // Remove map layer & source.
+      map.removeLayer(mapId);
+    }
+}
+
 
 
 // Display the data on the map as a layer
-function mapSourceAndLayer() {
+function mapSource() {
 	
 	let dataSelect = selectedSSP;
-	console.log("Data file : geo_calories_filtered_" + dataSelect + "cc.geojson");
+	//console.log("Data file : geo_calories_filtered_" + dataSelect + "cc.geojson");
 
-	//map.on('load', function () {
+	// Add source
+	map.addSource("earthquakes", {
+		type: "geojson",
+		// Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+		// from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+		data: "final" + dataSelect + ".geojson",
+		//data: "geo_calories_filtered_" + dataSelect + "cc.geojson",
+		cluster: false,
+		clusterMaxZoom: 6, // Max zoom to cluster points on
+		clusterRadius: 10 // Radius of each cluster when clustering points (defaults to 50)
+	});
+}
 
-		// Add source
-		map.addSource("earthquakes", {
-			type: "geojson",
-			// Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-			// from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-			//data: "geo_calories_filtered_SSP1cc.geojson",
-			data: "geo_calories_filtered_" + dataSelect + "cc.geojson",
-			cluster: false,
-			clusterMaxZoom: 6, // Max zoom to cluster points on
-			clusterRadius: 10 // Radius of each cluster when clustering points (defaults to 50)
-		});
+function mapLayer(subData) {
 
-		// Add layer
-		map.addLayer({
-			"id": "earthquakes",
-			"type": "heatmap",
-			"source": "earthquakes",
-			"maxzoom": 10,
-			"paint": {
-				// Increase the heatmap weight based on frequency and property magnitude
-				"heatmap-weight": [
-					"interpolate",
-					["linear"],
-					["get", "calories"],
-					22000, 0,
-					35000, 1
-				],
-				// Increase the heatmap color weight weight by zoom level
-				// heatmap-intensity is a multiplier on top of heatmap-weight
-				"heatmap-intensity": [
-					"interpolate",
-					["linear"],
-					["zoom"],
-					0, 1,
-					9, 10
-				],
-				// Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-				// Begin color ramp at 0-stop with a 0-transparancy color
-				// to create a blur-like effect.
-				"heatmap-color": [
-					"interpolate",
-					["linear"],
-					["heatmap-density"],
-					0, "rgba(33,102,172,0)",
-					0.02, "rgba(103,169,207,0.5)",
-					0.1, "rgba(209,229,240,0.5)",
-					0.14, "rgba(253,219,199,0.5)",
-					0.28, "rgba(239,138,98,0.5)",
-					1, "rgba(78,24,43,0.5)"
-				],
-				// Adjust the heatmap radius by zoom level
-				"heatmap-radius": [
-					"interpolate",
-					["linear"],
-					["zoom"],
-					0, 2,
-					8, 30
-				],
-			}
-		});
+	if (typeof(subData)==='undefined') subData = "calories";
+	//console.log("subData :" + subData);
 
-		hideLoading();
+	// Add layer
+	map.addLayer({
+		"id": "earthquakes",
+		"type": "heatmap",
+		"source": "earthquakes",
+		"maxzoom": 10,
+		"paint": {
+			// Increase the heatmap weight based on frequency and property magnitude
+			"heatmap-weight": [
+				"interpolate",
+				["linear"],
+				["get", subData],
+				22000, 0,
+				35000, 1
+			],
+			// Increase the heatmap color weight weight by zoom level
+			// heatmap-intensity is a multiplier on top of heatmap-weight
+			"heatmap-intensity": [
+				"interpolate",
+				["linear"],
+				["zoom"],
+				0, 1,
+				9, 10
+			],
+			// Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+			// Begin color ramp at 0-stop with a 0-transparancy color
+			// to create a blur-like effect.
+			"heatmap-color": [
+				"interpolate",
+				["linear"],
+				["heatmap-density"],
+				0, "rgba(33,102,172,0)",
+				0.02, "rgba(103,169,207,0.5)",
+				0.1, "rgba(209,229,240,0.5)",
+				0.14, "rgba(253,219,199,0.5)",
+				0.28, "rgba(239,138,98,0.5)",
+				1, "rgba(178,24,43,0.5)"
+			],
+			// Adjust the heatmap radius by zoom level
+			"heatmap-radius": [
+				"interpolate",
+				["linear"],
+				["zoom"],
+				0, 2,
+				8, 30
+			],
+		}
+	});
 	
-	//});
+	hideLoading();
+
+
 }
 
 function mapInteract() {
@@ -264,7 +278,8 @@ whenDocumentLoaded(() => {
 	showLoading();
 	// Bind the default scenario data to the map and add sourcer/layer
 	map.on('load', function () {
-		mapSourceAndLayer();
+		mapSource();
+		mapLayer();
 	});
 	
 	// Update the placeholder below the map
@@ -284,7 +299,8 @@ function onSSPchanged() {
 	updateSSP();
 
 	mapRemove();
-	mapSourceAndLayer();
+	mapSource();
+	mapLayer();
 
 	displayInfo();
 	console.log("");
@@ -334,18 +350,20 @@ var getLocalStorageSize = function() {
 ///////////////////////////////////////////////
 
 var checkboxes = document.getElementsByClassName("form-check-input");
-console.log("Checkboxes: " + checkboxes);
+var previousCheck = "calories";
+//console.log("Checkboxes: " + checkboxes);
 for(let i=0; i<checkboxes.length;i++){
 
     checkboxes[i].onclick=function(){
 
-      console.log("Current index: "+i);
+      if (checkboxes[i].checked && checkboxes[i].value != previousCheck) {
+      	showLoading();      	
+      	console.log("Update the map with : " + checkboxes[i].value);
+      	mapRemoveLayerOnly();
+      	mapLayer(checkboxes[i].value);
+      	previousCheck = checkboxes[i].value;
 
-      if (checkboxes[i].checked) {
-      	console.log( checkboxes[i].value + " has been checked");
 
-      } else {
-      	
-      }
+      } 
     };
 }
